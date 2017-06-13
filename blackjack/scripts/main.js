198647470cb1a86e59a943c2f3b3d6558bf1c632
EamonBachari <eamonbachari@gmail.com>
2017-06-13 12:17:07 -0500

    Complete Blackjack function.

--- blackjack/scripts/main.js
+++ blackjack/scripts/main.js
@@ -5,16 +5,43 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
 function handValue (hand) {
+  let aceCount = 0;
+  let sum = 0;
 
+  for(let i = 0; i < hand.length; ++i){
+    if(hand[i] == "A"){
+      ++aceCount;
+      sum = sum + 11;
+    }
+    else if(hand[i] == "K"){
+      sum = sum + 10;
+    }
+    else if(hand[i] == "Q"){
+      sum = sum + 10;
+    }
+    else if(hand[i] == "J"){
+      sum = sum + 10;
+    }
+    else{
+      sum = sum + (hand[i] * 1);
+    }
+  }
 
-  return;
+  while(aceCount > 0 && sum > 21){
+    sum = sum - 10;
+    --aceCount;
+  }
+
+  return sum;
 }
+
+console.log(handValue(["2", "2", "8"]));
 
 
 /* -----  Hints ------
